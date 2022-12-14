4f615f924cf2f53baea5ca19f9939c797837972a
vladtka <vladtka05@gmail.com>
2022-12-14 03:57:52 +0200

    session

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int Rec(int i, int n, int step, int current) {
+    return i < n ? Rec(i + 1, n, step, current + step) : current;
+}
+
+int main()
+{
+    int step = 4;
+    int n;
+    cout << "write n --> ";
+    cin >> n;
+    cout << Rec(1, n, step, 0) << "\n";
+    return(0);
+}
